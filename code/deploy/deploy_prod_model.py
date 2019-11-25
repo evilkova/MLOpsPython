7835735117762772f37c6e55914ba9e2032c56a4
evilkova <elena.vilkova@nl.abnamro.com>
2019-11-25 17:05:01 +0100

    get compute target from dotenv

--- code/deploy/deploy_prod_model.py
+++ code/deploy/deploy_prod_model.py
@@ -1,9 +1,47 @@
 from azureml.core.model import InferenceConfig
 from azureml.core.webservice import AksWebservice
-from azureml.core.compute import ComputeTarget
 from azureml.core import Run, Model
-from azure.common.credentials import ServicePrincipalCredentials
 import argparse
+import os
+import sys
+from dotenv import load_dotenv
+sys.path.append(os.path.abspath("./ml_service/util"))  # NOQA: E402
+from workspace import get_workspace
+from attach_compute import get_compute
+
+load_dotenv()
+workspace_name = os.environ.get("BASE_NAME")+"-AML-WS"
+resource_group = os.environ.get("RESOURCE_GROUP")
+subscription_id = os.environ.get("SUBSCRIPTION_ID")
+tenant_id = os.environ.get("TENANT_ID")
+app_id = os.environ.get("SP_APP_ID")
+app_secret = os.environ.get("SP_APP_SECRET")
+deploy_script_path = os.environ.get("DEPLOY_PROD_SCRIPT_PATH")
+vm_size = os.environ.get("AML_COMPUTE_CLUSTER_CPU_SKU")
+compute_name = os.environ.get("AML_COMPUTE_CLUSTER_NAME")
+model_name = os.environ.get("MODEL_NAME")
+build_id = os.environ.get("BUILD_BUILDID")
+pipeline_name = os.environ.get("DEPLOY_PROD_PIPELINE_NAME")
+service_name = os.environ.get("DEPLOY_PROD_SERVICE_NAME")
+sources_directory_train = os.environ.get("SOURCES_DIR_TRAIN")
+
+# Get Azure machine learning workspace
+aml_workspace = get_workspace(
+    workspace_name,
+    resource_group,
+    subscription_id,
+    tenant_id,
+    app_id,
+    app_secret)
+print(aml_workspace)
+
+# Get Azure machine learning cluster
+aml_compute = get_compute(
+    aml_workspace,
+    compute_name,
+    vm_size)
+if aml_compute is not None:
+    print(aml_compute)
 
 parser = argparse.ArgumentParser("deploy")
 parser.add_argument(
@@ -58,8 +96,6 @@ model = next(
     )
 )
 
-compute_target = ComputeTarget(workspace=ws, name="one-dspe-aks")
-print(compute_target)
 deployment_config = AksWebservice.deploy_configuration(cpu_cores=1,
                                                        memory_gb=4)
 service = Model.deploy(workspace=ws,
@@ -67,6 +103,6 @@ service = Model.deploy(workspace=ws,
                        models=[model],
                        inference_config=inference_config,
                        deployment_config=deployment_config,
-                       deployment_target=compute_target,
+                       deployment_target=aml_compute,
                        overwrite=True)
 service.wait_for_deployment(show_output=True)
